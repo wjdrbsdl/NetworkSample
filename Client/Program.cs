c479aefdab871f8691aea2d799edccc3eee1e0af
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-18 09:14:12 +0900

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+
+namespace Client
+{
+    class Messege
+    {
+        public string msg;
+
+        public Messege(string _msg)
+        {
+            msg = _msg;
+        }
+
+        ~Messege()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //서버에 붙을 소켓
+            for (int i = 0; i < 10; i++)
+            {
+                Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 4000);
+                serverSocket.Connect(endPoint);
+
+     
+                Messege clientMsg = new Messege("안녕하세요");
+                string json = JsonConvert.SerializeObject(clientMsg);
+                byte[] sendBuff = Encoding.UTF8.GetBytes(json);
+                int sendLength = serverSocket.Send(sendBuff);
+                //Console.WriteLine("보낸 길이 " + sendLength);
+
+                
+                byte[] recvBuff = new byte[1024];
+                int recvLength = serverSocket.Receive(recvBuff);
+                string resMsgStr = Encoding.UTF8.GetString(recvBuff);
+                Messege resMsg = JsonConvert.DeserializeObject<Messege>(resMsgStr);
+                Console.WriteLine($"서버가 보낸 메시지{resMsg.msg}");
+
+                serverSocket.Close();
+            }
+        }
+    }
+}
+
+
+/*
+ * 서버
+ * 소켓만들기 - 인터넷, ip4, tcp
+ * 소켓 바인드 - 아이피 주소와 내가만든 소켓을 연결
+ * 소켓 리슨 - 내가 만든 소켓 듣는 상태로 변경
+ * 소켓 어쎕 - 리슨 중에 들어오는 애 있을때까지 기다렸다가 받아줌 -> 반환 클라 소켓
+ * 반환된 클라 소켓에 - 센드 혹은 리십
+ * 
+ * 클라
+ * 소켓 만들기 - 인터넷, ip4, tcp
+ * 서버로 소켓 연결 - (안에 목적지 endPoint 만들어서 넣기)
+ * 서버로 소켓 센드, 리십
+ */
---
+++ Server/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Messege
+    {
+        public string msg;
+
+        public Messege(string _msg)
+        {
+            msg = _msg;
+        }
+
+        ~Messege()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint listenEndPoint = new IPEndPoint(
+                IPAddress.Any //랜카드 여러개중 아무거나
+                ,4000  //해당 프로그램에 할당할 논리적 주소
+                );
+            socket.Bind(listenEndPoint); //socket에 주소(랜카드, 포트)를 연결
+            socket.Listen(10); //이제 들어오는 정보를 듣고거다. 몇개까지?
+
+            while (true)
+            {
+                //동기 방식
+                Socket clientSocket = socket.Accept(); //손놈과 연결된 소켓 - 여기 포트번호는 랜덤으로 설정되어있음
+
+                byte[] receiveBuffer = new byte[1024];
+                int recvLength = clientSocket.Receive(receiveBuffer); //받으면 길이부터 줌
+                if (recvLength == 0)
+                {
+                    //닫은거
+                }
+                else if (recvLength < 0)
+                {
+
+                    //에러
+                }
+                string resMsgStr = Encoding.UTF8.GetString(receiveBuffer);
+                Messege resMsg = JsonConvert.DeserializeObject<Messege>(resMsgStr);
+                Console.WriteLine($"클라가 보낸 메시지{resMsg.msg}");
+
+                Messege sendMsg = new Messege("반가워요");
+                string sendJson = JsonConvert.SerializeObject(sendMsg);
+                byte[] sendData = Encoding.UTF8.GetBytes(sendJson);
+                int sendLength = clientSocket.Send(sendData);
+                //sendLength가 0 이면 상대쪽이 끊은거
+                //0 아래면 어딘진 몰라도 에러
+
+                //이거 안하면 keep alive time();?? 보통 3분?
+                clientSocket.Close();
+            }
+            
+        }
+    }
+}
