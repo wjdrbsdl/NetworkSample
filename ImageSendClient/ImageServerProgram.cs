440949e160ce199cc93068fb0cec06bf095a71d4
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-18 17:36:53 +0900

    [추가] 이미지 주고 받기

---
+++ ImageSendClient/ImageServerProgram.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace ImageSendClient
+{
+    internal class ImageServerProgram
+    {
+        static void Main(string[] args)
+        {
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint listenEndPoint = new IPEndPoint(
+                IPAddress.Any //랜카드 여러개중 아무거나
+                , 4000  //해당 프로그램에 할당할 논리적 주소
+                );
+            listenSocket.Bind(listenEndPoint); //socket에 주소(랜카드, 포트)를 연결
+            listenSocket.Listen(10); //이제 들어오는 정보를 듣고거다. 몇개까지?
+
+            Socket clientSocket = listenSocket.Accept();
+
+            FileStream fsInput = new FileStream("1.webp", FileMode.Open);
+            byte[] readBuffer = new byte[1000];
+
+            int readSize = 0;
+            do 
+            {
+                readSize = fsInput.Read(readBuffer, 0, readBuffer.Length);
+                int sendSize = clientSocket.Send(readBuffer, readSize, SocketFlags.None);
+            }
+            while (readSize > 0);
+
+            fsInput.Close();
+            listenSocket.Close();
+            clientSocket.Close();
+        }
+    }
+}
---
+++ ImageSendServer/ImageClientProgram.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Net.Security;
+
+namespace ImageSendServer
+{
+    public class ImageClientProgram
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 4000);
+            serverSocket.Connect(endPoint);
+
+            FileStream fsOutput = new FileStream("3_copy.webp", FileMode.Create);
+            //열었으면 무조건 닫기. 
+
+            int recv = 0;
+            do
+            {
+                byte[] recvBuff = new byte[100];
+                recv = serverSocket.Receive(recvBuff);
+                if (recv <= 79)
+                {
+
+                }
+                fsOutput.Write(recvBuff, 0, recv);
+                Console.WriteLine("받은 숫자 "+recv);
+            } while (recv>= 80);
+
+
+            fsOutput.Close();
+        }
+    }
+}
