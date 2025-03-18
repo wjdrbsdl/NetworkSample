145346d06d227f9f1bbed9220ae5958eeca8b4ff
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-18 17:36:34 +0900

    [추가]  udp 통신 연습

---
+++ UdpClient/UdpClient.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+
+public class UdpClient
+{
+    static void Main(string[] args)
+    {
+        //Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        //IPEndPoint svEndPoint = new IPEndPoint(IPAddress.Loopback, 6000);//목적지
+
+        //byte[] buffer = new byte[1024]; //상대가 이것보다 큰거 보내면 나머진 못받음? 에러남. -
+        //  //-- C로 보내면 에러는 안나고 멈춤. 
+        //string message = "안녕ㅎ";
+        //buffer = Encoding.UTF8.GetBytes(message);
+        //int sendLength = serverSocket.SendTo(buffer, buffer.Length, SocketFlags.None, svEndPoint);
+
+        //byte[] buffer2 = new byte[1024];
+        //EndPoint remotePoint = svEndPoint;
+        //int recv = serverSocket.ReceiveFrom(buffer2, ref remotePoint);
+
+        //string message2 = Encoding.UTF8.GetString(buffer);
+        //Console.WriteLine(message2);
+
+        //serverSocket.Close();
+
+        IPHostEntry hostEntry = Dns.GetHostEntry("naver.com");
+        foreach (IPAddress item in hostEntry.AddressList)
+        {
+
+        }
+    }
+}
---
+++ UdpServer/UdpServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+public class UdpServer
+{
+    static void Main(string[] args)
+    {
+        //udp 형태로 만들어놓음. 
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint svEndPoint = new IPEndPoint(IPAddress.Any, 6000);
+        serverSocket.Bind(svEndPoint);
+
+        byte[] buffer = new byte[1024]; //상대가 이것보다 큰거 보내면 나머진 못받음?
+        EndPoint clientEnPoint = (EndPoint)svEndPoint;
+        int recvLength = serverSocket.ReceiveFrom(buffer, ref clientEnPoint);
+        int sendLength = serverSocket.SendTo(buffer, clientEnPoint);
+        serverSocket.Close();
+    }
+}
+
